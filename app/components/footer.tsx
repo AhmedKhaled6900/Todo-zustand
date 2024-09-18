02873f6da5f7f606c42401c4ebb03a050e831abc
AhmedKhaled6900 <you@example.com>
2024-09-18 18:50:22 +0300

    add footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -11,71 +11,70 @@ import { FaInstagram } from "react-icons/fa";
 
 import { FaLinkedin } from "react-icons/fa6";
 <FaLinkedin />
+import { FaWhatsapp } from "react-icons/fa";
+import { CiLocationOn, CiMail } from "react-icons/ci";
+import { IoCallOutline } from "react-icons/io5";
+import { TbClockHour3 } from "react-icons/tb";
+<FaWhatsapp />
 const Footer = () => {
     const router =useRouter()
+
+
     const social = [
         {
     name : "facebook",
     href:"www.facebook.com",
     icon :<FaFacebook size={30} />
-
     },
         {
     name : "twitter",
     href:"www.twitter.com",
     icon :<BsTwitterX size={30} />
-
     },
         {
     name : "linkedin",
     href:"www.linkedin.com",
     icon :<FaLinkedin size={30} />
-
     },
         {
     name : "instagram",
     href:"www.instagram.com",
     icon :<FaInstagram size={30} />
-
     },
     
 ]
     return ( 
-
-        <div className="flex items-center flex-col justify-center mt-10 pt-10 bg-green-500">
+        <div className="flex items-center flex-col justify-center mt-10 pt-10 bg-sky-800">
 <h1>
-© Copyright ComingSoon All Rights Reserved
+© Copyright Todo All Rights Reserved
 </h1>
 
 <div className="animated-text text-center flex flex-col mt-5">
     <h1 className="text-xl font-semibold">
-        Made by 
-   
+        Made by    
     </h1>
-    <span className="text-3xl font-bold">
+    <span className="text-3xl mt-4 font-bold">
         Ahmed Khaled
     </span>
    
 </div>
-<div className="mt-5 ">
-    <h1 className="text-xl font-semibold mt-5">
+<div className="mt-4 ">
+    <h1 className="text-xl font-semibold ">
         Follow me
     </h1>
 </div>
-<div className="flex items-center justify-between my-10 gap-x-6 ">
+<div className="flex items-center justify-between my-4 gap-x-3 lg:gap-x-6 ">
     {
         social.map((item)=>(
                 <a href={item.href} className="hover:text-white" target="_blank" rel="noopener noreferrer">
             <button key={item.href}  >
             {item.icon}
-
             </button>
-
                 </a>
-
         ))
     }
 </div>
+
         </div>
      );
 }
